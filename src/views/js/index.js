532bdb380af5b30baa907ad05fc0fe7a7c5bcfb5
Johan Puentes <puentespulidojohansebastian@gmail.com>
2024-05-01 20:42:09 -0500

    on,once,off

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,22 +1,12 @@
 // its not necessary that you create a new variable with
 // htmlNode, when you create a new element with id selector,
 // the browser create a new global var
-const button = document.getElementById('msg');
 const socket = io();
-function checkSocketStatus() {
-    console.log(`%csocket status: ${socket.connected ? 'connected' : 'disconnected'}`,"color:green;font-family:system-ui;font-size:2rem;font-weight:bold;");
-}
-
-socket.on('hello', (e) => {
-    console.log(e);
-    checkSocketStatus();
-});
-
+const emitToLast = document.getElementById('emit-to-last');
 socket.on('everyone',msg => text.textContent = msg);
 
 socket.on('disconnect',()=> {
     console.log("the next socket have been disconnected",socket.id);
-    checkSocketStatus();
 });
 
 socket.on('connect_error',()=> {
@@ -31,11 +21,32 @@ socket.io.on('reconnect',()=> {
 
 socket.io.on('reconnect_attempt',()=> {
     console.log("%cim trying to reconnect🧠❔","color:gray;font-family:system-ui;font-size:2rem;font-weight:bold;");
-    checkSocketStatus();
 });
 
-button.addEventListener('click', () => {
-    socket.emit("ping", "pong");
-    console.log("ping");
-    checkSocketStatus();
+emitToLast.addEventListener('click',(e) => {
+    e.preventDefault();
+    socket.emit("last","hello from "+socket.id);
 });
+
+socket.on('greeting',(msg)=> {
+    text.textContent = msg;
+});
+
+// on,once,off
+socket.on('on',(_) => {
+    console.log("an event that is emitted many times");
+});
+
+socket.once('once',() => {
+    console.log("there are 2 events but this message gonna appear one time");
+});
+
+const listener = () => {
+    console.log("the event turn off");
+};
+
+socket.on('off',listener);
+
+setTimeout(() => {
+    socket.off('off',listener);
+}, 2000);
